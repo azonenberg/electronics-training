c9e00b1418e463c34ec8d4df1638f86ec5e89b0b
Andrew D. Zonenberg <azonenberg@drawersteak.com>
2026-09-04 01:17:05 -0700

    Began initial skeleton of demo board firmware

---
+++ oscilloscope-probing/lab/firmware/demo-board/super/bsp/hwinit.h
@@ -0,0 +1,57 @@
+/***********************************************************************************************************************
+*                                                                                                                      *
+* electronics-training                                                                                                 *
+*                                                                                                                      *
+* Copyright (c) 2023-2026 Andrew D. Zonenberg and contributors                                                         *
+* All rights reserved.                                                                                                 *
+*                                                                                                                      *
+* Redistribution and use in source and binary forms, with or without modification, are permitted provided that the     *
+* following conditions are met:                                                                                        *
+*                                                                                                                      *
+*    * Redistributions of source code must retain the above copyright notice, this list of conditions, and the         *
+*      following disclaimer.                                                                                           *
+*                                                                                                                      *
+*    * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the       *
+*      following disclaimer in the documentation and/or other materials provided with the distribution.                *
+*                                                                                                                      *
+*    * Neither the name of the author nor the names of any contributors may be used to endorse or promote products     *
+*      derived from this software without specific prior written permission.                                           *
+*                                                                                                                      *
+* THIS SOFTWARE IS PROVIDED BY THE AUTHORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED   *
+* TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL *
+* THE AUTHORS BE HELD LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES        *
+* (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR       *
+* BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT *
+* (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE       *
+* POSSIBILITY OF SUCH DAMAGE.                                                                                          *
+*                                                                                                                      *
+***********************************************************************************************************************/
+
+#ifndef hwinit_h
+#define hwinit_h
+
+#include <peripheral/ADC.h>
+#include <peripheral/EXTI.h>
+#include <peripheral/Flash.h>
+#include <peripheral/GPIO.h>
+#include <peripheral/I2C.h>
+#include <peripheral/SPI.h>
+#include <peripheral/UART.h>
+
+#include <supervisor/supervisor-common.h>
+
+///@brief Initialize application-specific hardware stuff
+extern void App_Init();
+
+//Common ISRs used by application and bootloader
+void USART1_Handler();
+
+//GPIOs
+extern GPIOPin g_pgoodLED;
+extern GPIOPin g_faultLED;
+extern GPIOPin g_sysokLED;
+
+void InitGPIOs();
+float GetLTCTemp();
+
+#endif
